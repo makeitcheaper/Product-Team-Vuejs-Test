eb09b242f66ae5d3d49ee6d1aaebcab2780b6f69
Ger Carney <Ger@noclipdigital.com>
2019-06-20 10:49:52 +0100

    Set up App component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      conversation: [
+        {
+          sender: 'bot',
+          text: "Hello! Please choose from one of the options below:"
+        },
+        {
+          sender: 'user',
+          options: [
+            'Option A',
+            'Option B',
+            'Option C'
+          ]
+        },
+        {
+          sender: 'bot',
+          text: "Option A? Great choice!"
+        },
+      ]
+    }
   }
 }
 </script>
 
 <style lang="scss">
+body, html {
+  height: 100%;
+  margin: 0;
+  background-color: #F7F8F9;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  padding-top: 60px;
+  height: 100%;
 }
+
 </style>
