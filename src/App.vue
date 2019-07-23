0d0c538ecb8ae6e6f4cf98f2dee0ce2cc2cf512c
Ger Carney <Ger@noclipdigital.com>
2019-07-23 09:19:42 +0100

    Removed starter scss

--- src/App.vue
+++ src/App.vue
@@ -11,27 +11,12 @@ export default {
 
   data() {
     return {
-      
+
     }
   }
 }
 </script>
 
 <style lang="scss">
-body, html {
-  height: 100%;
-  margin: 0;
-  background-color: #F7F8F9;
-}
-
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  padding-top: 60px;
-  height: 100%;
-}
 
 </style>
